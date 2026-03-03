7897e680ca325c35051b7bc2b50dd74cff723f64
ambikesh587036 <ambikeshdwivedi957@gmail.com>
2026-03-03 15:38:13 +0530

    Enhance Navbar with background and shadow styles
    Updated Navbar component styles with a background and shadow.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -24,7 +24,7 @@ function Navbar() {
   };
 
   return (
-    <div className="flex items-center justify-between py-5 font-medium">
+    <div className="flex items-center justify-between py-5 font-medium bg-black text-white px-6 shadow-lg">
       <Link to={"/"}>
         <img src={assets.logo} className="w-36" alt="logoImage" />
       </Link>
