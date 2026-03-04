9cd32fb61f374a068a0f0f75ed45621d953afdbb
ambikesh587036 <ambikeshdwivedi957@gmail.com>
2026-03-04 09:51:17 +0530

    Refactor server setup and update root response
    Removed database and cloudinary connections, along with related routes. Updated the success message for the root endpoint.

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './config/mongodb.js';
-import connectCloudinary from './config/cloudinary.js';
-import userRouter from './routes/userRoute.js';
-import productRouter from './routes/productRoute.js';
-import cartRouter from './routes/cartRoute.js';
-import orderRouter from './routes/orderRoute.js';
 
 const app = express();
 
-// connect database & cloudinary
-connectDB();
-connectCloudinary();
-
-// middlewares
 app.use(express.json());
 app.use(cors());
 
-// api endpoints
-app.use('/api/user', userRouter);
-app.use('/api/product', productRouter);
-app.use('/api/cart', cartRouter);
-app.use('/api/order', orderRouter);
-
 app.get('/', (req, res) => {
-    res.send('API Is Working Properly');
+  res.send('Backend is running successfully 🚀');
 });
 
 export default app;
