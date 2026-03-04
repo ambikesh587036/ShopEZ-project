452c7efed3bc407b7b729f9ba5d7f7f72c199bf0
ambikesh587036 <ambikeshdwivedi957@gmail.com>
2026-03-04 10:21:16 +0530

    Update server response message and add configurations

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,33 @@
+
+
 import express from 'express';
 import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
+import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 const app = express();
 
+// connect database & cloudinary
+connectDB();
+connectCloudinary();
+
+// middlewares
 app.use(express.json());
 app.use(cors());
 
+// api endpoints
+app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
+
 app.get('/', (req, res) => {
-  res.send('Backend is running successfully 🚀');
+    res.send('API Is Working Properly');
 });
 
 export default app;
