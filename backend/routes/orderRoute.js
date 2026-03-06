7073489e03d44ea5f7869764be6cd200ee5b6b5b
ambikesh587036 <ambikeshdwivedi957@gmail.com>
2026-03-06 16:51:19 +0530

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,30 +1,21 @@
 import express from 'express';
-import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe,verifyRazorpay} from '../controllers/orderController.js';
+import { placeOrder, allOrders, userOrders, updateStatus } from '../controllers/orderController.js';
 import adminAuth from '../middleware/adminAuth.js';
 import authUser from '../middleware/auth.js';
-
-
 
 const orderRouter = express.Router();
 
 
 // admin features
-orderRouter.post('/list', adminAuth,allOrders);
+orderRouter.post('/list', adminAuth, allOrders);
 orderRouter.post('/status', adminAuth, updateStatus);
 
 
-// payment features
-orderRouter.post('/place', authUser,placeOrder);
-orderRouter.post('/stripe', authUser, placeOrderStripe);
-orderRouter.post('/razorpay', authUser, placeOrderRazorpay);
+// place order (COD only)
+orderRouter.post('/place', authUser, placeOrder);
 
 
 // user features
 orderRouter.post('/userorders', authUser, userOrders);
-
-
-// verify payment
-orderRouter.post('/verifyStripe', authUser, verifyStripe);
-orderRouter.post('/verifyRazorpay', authUser, verifyRazorpay);
 
 export default orderRouter;
